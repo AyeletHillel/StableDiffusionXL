d119bc4f6e4d20cecbe0f44e07925f5a70957a64
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-05-11 18:51:52 -0400

    generateStableDiffusionImage

--- stable.py
+++ stable.py
@@ -19,35 +19,27 @@ def getModelList():
         print(payload)
 
 
-response = requests.post(
-    f"{api_host}/v1/generation/{engine_id}/text-to-image",
-    headers={
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    },
-    json={
-        "text_prompts": [
-            {
-                "text": "A lighthouse on a cliff"
-            }
-        ],
-        "cfg_scale": 7,
-        "clip_guidance_preset": "FAST_BLUE",
-        "height": 512,
-        "width": 512,
-        "samples": 1,
-        "steps": 30,
-    },
-)
-
-if response.status_code != 200:
-    raise Exception("Non-200 response: " + str(response.text))
-
-data = response.json()
-
-for i, image in enumerate(data["artifacts"]):
-    with open(f"./out/v1_txt2img_{i}.png", "wb") as f:
-        f.write(base64.b64decode(image["base64"]))
+def generateStableDiffusionImage(prompt, height, width, steps):
+    url = f"{api_host}/v1/generation/{engine_id}/text-to-image"
+    headers = {"Content-Type": "application/json", "Accept": "application/json","Authorization": f"Bearer {api_key}"}
+    
+    payload = {}
+    payload['text_prompts'] = [{ "text": f"{prompt}"}]
+    payload['cfg_scale'] = 7
+    payload['clip_guidance_preset'] = "FAST_BLUE"
+    payload['height'] = height
+    payload['width'] = width
+    payload['samples'] = 1
+    payload['steps'] = steps
+    
+    response = requests.post(url, headers=headers,json=payload)
+    
+    if response.status_code == 200:
+        data = response.json()        
+        for i, image in enumerate(data["artifacts"]):
+            with open(f"v1_txt2img_{i}.png", "wb") as f:
+                f.write(base64.b64decode(image["base64"]))
 
 
+prompt = 'Create a high resolution picture image of a luxury car in a studio setting udio setting, showcasting its lines and high-end features. Perfect lighting with highlights.'
+generateStableDiffusionImage(prompt, 512, 512, 15)
